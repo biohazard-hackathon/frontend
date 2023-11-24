8fa10aeec175e3c40b423656ea052c85f1e2ec7b
tinazhouhui <tina.zhou.hui@gmail.com>
2023-11-24 14:08:46 +0100

    + cognito

--- app/app.tsx
+++ app/app.tsx
@@ -1,25 +1,54 @@
-import React, { Component } from 'react';
-import { Provider as ReduxProvider } from 'react-redux';
-import { I18nextProvider } from 'react-i18next';
+import React, {FC} from 'react';
+import {Provider as ReduxProvider} from 'react-redux';
+import {I18nextProvider} from 'react-i18next';
 
 import i18n from './locales';
 import RootRouter from './router/RootRouter';
 import store from './store/configureStore';
+
+import {Amplify} from 'aws-amplify';
+import {
+	AWS_REGION,
+	HOSTED_UI_DOMAIN,
+	REDIRECT_SIGN_IN,
+	REDIRECT_SIGN_OUT,
+	USER_POOL_ID,
+	USER_POOL_WEB_CLIENT_ID,
+} from './constants/env';
 
 interface Props {
 }
 
 interface State {
 }
+const App: FC<Props> = (props) => {
+	Amplify.configure({
+		Auth: {
+			region: AWS_REGION,
+			userPoolId: USER_POOL_ID,
+			userPoolWebClientId: USER_POOL_WEB_CLIENT_ID,
+			mandatorySignIn: true,
+			authenticationFlowType: 'USER_PASSWORD_AUTH',
+			oauth: {
+				domain: HOSTED_UI_DOMAIN,
+				scope: [
+					'openid',
+					'aws.cognito.signin.user.admin',
+					'email',
+				],
+				redirectSignIn: REDIRECT_SIGN_IN,
+				redirectSignOut: REDIRECT_SIGN_OUT,
+				responseType: 'code', // or 'token', note that REFRESH token will only be generated when the responseType is code
+			},
+		},
+	});
+	return <React.StrictMode>
+		<ReduxProvider store={store}>
+			<I18nextProvider i18n={i18n}>
+				{<RootRouter {...props} />}
+			</I18nextProvider>
+		</ReduxProvider>
+	</React.StrictMode>;
+};
 
-export default class App extends Component<Props, State> {
-	render() {
-		return <React.StrictMode>
-			<ReduxProvider store={store}>
-				<I18nextProvider i18n={i18n}>
-					{<RootRouter {...this.props} />}
-				</I18nextProvider>
-			</ReduxProvider>
-		</React.StrictMode>;
-	}
-}
+export default App;
--- app/pages/HomePage.tsx
+++ app/pages/HomePage.tsx
@@ -1,21 +1,52 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import Icon from "../components/Icon";
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
+import {Auth} from 'aws-amplify';
 
 interface Props {
 }
 
-interface State {
+interface IUserInfo {
+	attributes: {
+		sub: string,
+		email: string,
+		email_verified: boolean,
+		name: string,
+	},
+	id: string | undefined,
+	username: string
 }
 
-export const HomePage: FC<Props> = ({  }) => {
-	const { t } = useTranslation();
+export const HomePage: FC<Props> = ({}) => {
+	const [isSignedIn, setSignIn] = useState<boolean>(false);
+	const [user, setUser] = useState<IUserInfo>();
+	useEffect(() => {
+		(async () => {
+			try {
+				const userInfo = await Auth.currentUserInfo() as IUserInfo;
+				if (user) {
+					setSignIn(true);
+					setUser(userInfo);
+
+				} else {
+					console.log('user not signed in');
+					await Auth.federatedSignIn();
+				}
+			} catch (error: unknown) {
+				console.error(error);
+				// todo redirect to error page
+			}
+		})();
+
+	}, [isSignedIn]);
+
+	const {t} = useTranslation();
 	return <h1 className="text-center">
-			<Icon name="biohazard" spin fixedWidth/>
-			&nbsp;
-			{t('title')}
-		</h1>;
-}
+		<Icon name="biohazard" spin fixedWidth/>
+		&nbsp;
+		{t('title')}
+	</h1>;
+};
 
 
 export default HomePage;
