248207be997660b709f6b241f284ed2616589b4c
tinazhouhui <tina.zhou.hui@gmail.com>
2023-11-24 19:52:27 +0100

    / env variables in config

--- app/constants/env.ts
+++ app/constants/env.ts
@@ -1,26 +1,13 @@
 const rootElement = document.getElementById('root');
 
-function resolveApiUrl(external = process.env.API_URL): string {
-	if (external) {
-		return external;
-	}
+export const PUBLIC_URL = rootElement?.getAttribute('data-public-url') ?? '';
 
-	if (rootElement) {
-		return rootElement.getAttribute('data-api-url') || '';
-	}
+export const VERSION = rootElement?.getAttribute('data-version') ?? '';
 
-	return '';
-}
-
-export const PUBLIC_URL: string = rootElement ? rootElement.getAttribute('data-public-url') || '' : '';
-
-export const VERSION: string = rootElement ? rootElement.getAttribute('data-version') || '' : '';
-
-export const API_URL: string = resolveApiUrl();
-
-export const AWS_REGION = '';
-export const HOSTED_UI_DOMAIN = '';
-export const REDIRECT_SIGN_IN = '';
-export const REDIRECT_SIGN_OUT = '';
-export const USER_POOL_ID = '';
-export const USER_POOL_WEB_CLIENT_ID = '';
+export const API_URL = rootElement?.getAttribute('api-url') ?? '';
+export const AWS_REGION = 'us-east-1';
+export const HOSTED_UI_DOMAIN = rootElement?.getAttribute('data-hosted-ui-domain') ?? '';
+export const REDIRECT_SIGN_IN = rootElement?.getAttribute('data-redirect-sign-in') ?? '';
+export const REDIRECT_SIGN_OUT = rootElement?.getAttribute('data-redirect-sign-out') ?? '';
+export const USER_POOL_ID = rootElement?.getAttribute('data-user-pool-id') ?? '';
+export const USER_POOL_WEB_CLIENT_ID = rootElement?.getAttribute('data-user-pool-web-client-id') ?? '';
