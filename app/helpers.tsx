2642e08be4c6fc01fc7d7986e3f0f6f92c906e92
tinazhouhui <tina.zhou.hui@gmail.com>
2023-11-25 15:30:36 +0100

    + create uuid

--- app/helpers.tsx
+++ app/helpers.tsx
@@ -10,7 +10,7 @@ export const refreshToken = () => {
 			console.error(error);
 		}
 	})();
-}
+};
 
 export const authenticateUser = (user: IUserInfo | undefined, setSignIn: Function, setUser: Function) => {
 	(async () => {
@@ -19,7 +19,7 @@ export const authenticateUser = (user: IUserInfo | undefined, setSignIn: Functio
 			if (user) {
 				const userData = await Auth.currentAuthenticatedUser();
 				console.log('userData', userData);
-				localStorage.setItem('token', userData?.signInUserSession.refreshToken.token)
+				localStorage.setItem('token', userData?.signInUserSession.refreshToken.token);
 				setSignIn(true);
 				setUser(userInfo);
 			} else {
@@ -31,4 +31,19 @@ export const authenticateUser = (user: IUserInfo | undefined, setSignIn: Functio
 			// todo redirect to error page
 		}
 	})();
-}
+};
+export const createUUID = () => {
+    // http://www.ietf.org/rfc/rfc4122.txt
+    const uuid = [];
+    const hexDigits = "0123456789abcdef";
+    for (let i = 0; i < 36; i++) {
+        uuid[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+    }
+    uuid[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+	uuid[19] = hexDigits.substr((uuid[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+    uuid[8] = uuid[13] = uuid[18] = uuid[23] = "-";
+
+    return uuid.join("");
+};
